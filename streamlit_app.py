ec34fc19783d2e3515f120c38a248be2699ad646
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-12 11:33:29 -0700

    Testing import and extraction of one monster from monsters.json

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import pandas as pd
+monsters = {}
+with open('monsters.json','r') as m:
+    monsters = m
+st.write(m['aboleth']
+st.title("test")
