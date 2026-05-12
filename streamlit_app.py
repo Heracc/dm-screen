9f8435e7c5d0b9fe3a706745198b90a8c738285e
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-12 11:36:18 -0700

    Fixed global variable stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 monsters = {}
 with open('monsters.json','r') as m:
+    global monsters
     monsters = m
-st.write(m['aboleth'])
+st.write(monsters['aboleth'])
 st.title("test")
