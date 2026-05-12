920d6c58541baac5412d2d3eb046772ad498e905
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-12 12:48:33 -0700

    Add bestiary, initiative, and players pages and sidebar for navigbation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,18 @@
 import streamlit as st
 import pandas as pd
 import json
+
+bestiary_page = st.Page("bestiary.py", title="Bestiary")
+initiative_page = st.Page("initiative.py", title="Initative Tracker)
+players_page = st.Page("players.py", title="Players")
+
+pg = st.navigation([bestiary_page, initative_page, players_page])
+pg.run()
+"""
 with open('monsters.json','r') as m:
     global monsters
     monsters = json.load(m)
 st.write(monsters['aboleth']['languages'][0])
 st.write(monsters['aboleth'])
 st.title("test")
+"""
