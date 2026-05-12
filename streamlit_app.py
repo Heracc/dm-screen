b4ce10485cf383d41e2016f17f546acbab74960a
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-12 11:51:05 -0700

    Testing retrieval of stats within a monster

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ import json
 with open('monsters.json','r') as m:
     global monsters
     monsters = json.load(m)
+st.write(monsters['aboleth']['languages'][0]
 st.write(monsters['aboleth'])
 st.title("test")
