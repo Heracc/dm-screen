f3ffe6cd5409efc1aed3472b9c9e15e56031be7f
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-13 16:18:51 -0700

    image width

--- pages/03bestiary.py
+++ pages/03bestiary.py
@@ -25,7 +25,7 @@ if is_valid(search):
         type_alignment = f"{monsters[search]['size']} {monsters[search]['creatureType']}, {monsters[search]['alignment']}"
         st.write(type_alignment.title())
         "---"
-        st.image(monsters[search]['imageUrl'], width=400)
+        st.image(monsters[search]['imageUrl'])
     with right:
         st.markdown(f"Challenge Rating: {monsters[search]['challenge']['rating']} ({monsters[search]['challenge']['xp']} XP)")
 else: 
