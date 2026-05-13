b0cdc1d0945742864e5b3523185466b7fbb1fd18
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-12 21:04:44 -0700

    searches json file now

--- pages/03bestiary.py
+++ pages/03bestiary.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+
 st.title("bestiary")
-search = st.text_input('search for a monster')
-st.write(search)
+search = st.text_input('search for a monster').lower().strip()
+with open('data/monsters.json','r') as monsters:
+    if search in monsters:
+        df = pandas.DataFrame(monsters[search])
+        st.dataframe(df)
+    else: st.write()
