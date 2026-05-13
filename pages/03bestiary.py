3bfda6fadcd0928d23e3ad0080c74f58d2444b00
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-12 21:28:49 -0700

    tryna fix the search... also fixed not in monsters message

--- pages/03bestiary.py
+++ pages/03bestiary.py
@@ -1,10 +1,7 @@
 import streamlit as st
-import pandas as pd
-
 st.title("bestiary")
 search = st.text_input('search for a monster').lower().strip()
 with open('data/monsters.json','r') as monsters:
     if search in monsters:
-        df = pandas.DataFrame(monsters[search])
-        st.dataframe(df)
-    else: st.write()
+        st.dataframe(monsters[search])
+    else: st.write(f"{search} is not a valid monster!")
