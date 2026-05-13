653c32ce346c393c3a4d1a0f851f4beef9d60ee0
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-12 21:31:20 -0700

    actually pulled the data out of the json

--- pages/03bestiary.py
+++ pages/03bestiary.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import json
 st.title("bestiary")
 search = st.text_input('search for a monster').lower().strip()
-with open('data/monsters.json','r') as monsters:
+with open('data/monsters.json','r') as file:
+    monsters = json.load(file)
     if search in monsters:
         st.dataframe(monsters[search])
     else: st.write(f"{search} is not a valid monster!")
