6df7972a3ad9f77b3a0ec435c7d2a643ac22a735
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-13 08:05:01 -0700

    testing columns

--- pages/03bestiary.py
+++ pages/03bestiary.py
@@ -1,9 +1,28 @@
 import streamlit as st
 import json
-st.title("bestiary")
-search = st.text_input('search for a monster').lower().strip()
+
 with open('data/monsters.json','r') as file:
+    global monsters
     monsters = json.load(file)
+
+def is_valid(search):
     if search in monsters:
-        st.dataframe(monsters[search])
-    else: st.write(f"{search} is not a valid monster!")
+        return True
+        st.write("test")
+    else: return False
+
+st.title("bestiary")
+
+search = st.text_input('search for a monster').lower().strip()
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    is_valid(search)
+
+with col3:
+    is_valid(search)
+    #st.image()
+
+if search in monsters:
+    st.dataframe(monsters[search])
+else: st.write(f"{search} is not a valid monster!")
