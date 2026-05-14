3beaa76e6203ba24f3e508e4edf5c39881f7faed
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-14 14:02:09 -0700

    syntax fix

--- pages/01initiative.py
+++ pages/01initiative.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("initiative")
 
-with st.container(horizontal=True), horizontal_alignment="distribute":
+with st.container(horizontal=True, horizontal_alignment="distribute"):
     st.text_input("add a monster or player", label_visibility="hidden", placeholder="Add a monster or player")
     st.space()
     st.button("Add Combatant")
