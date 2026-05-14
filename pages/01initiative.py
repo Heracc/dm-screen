2be93bed1331266b18d76325fddbeb6fdf9e403c
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-14 11:48:17 -0700

    resizing

--- pages/01initiative.py
+++ pages/01initiative.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("initiative")
 
-left, right = st.columns(2)
+left, right = st.columns([2,1])
 with left:
     st.text_input("add a monster or player", label_visibility="hidden", placeholder="Add a monster or player")
 
