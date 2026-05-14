1ac7907c62128565a73fb7a86b7dccfe03dd9c76
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-14 13:55:15 -0700

    align plz

--- pages/01initiative.py
+++ pages/01initiative.py
@@ -1,10 +1,7 @@
 import streamlit as st
 st.title("initiative")
 
-left, right = st.columns([2,1])
-with left:
+with st.container(horizontal=True):
     st.text_input("add a monster or player", label_visibility="hidden", placeholder="Add a monster or player")
-
-with right:
     st.space()
     st.button("Add Combatant")
