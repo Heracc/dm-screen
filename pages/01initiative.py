0f37cf56f4513b5ed67cb14b406ec678cff0024e
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-14 14:03:51 -0700

    align?

--- pages/01initiative.py
+++ pages/01initiative.py
@@ -3,5 +3,5 @@ st.title("initiative")
 
 with st.container(horizontal=True, horizontal_alignment="distribute"):
     st.text_input("add a monster or player", label_visibility="hidden", placeholder="Add a monster or player")
-    st.space()
+
     st.button("Add Combatant")
