fbb034a3cb05e511ee2397351c9c1754fc5ba408
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-15 09:43:42 -0700

    player add form

--- pages/02players.py
+++ pages/02players.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import json
+all_classes = ["Artificer","Barbarian","Bard","Cleric","Druid","Fighter","Monk","Paladin","Ranger","Rogue","Sorcerer","Warlock","Wizard"]
 if "players" not in st.session_state:
     st.session_state.players = {}
 st.title("players")
@@ -14,5 +15,13 @@ def form_callback():
 with st.expander("Add a Player"):
     with st.form("add_player", clear_on_submit=True, enter_to_submit=False):
         st.text_input("Character Name", placeholder="Character Name", label_visibility="hidden", key="name_input")
-        st.number_input("Armor Class", placeholder="Armor Class", label_visibility="hidden", key="ac_input")
+        st.text_input("Race", placeholder="Race", label_visibility="hidden", key="race_input")
+        st.selectbox("Class", all_classes, placeholder="Class", label_visibility="hidden", index=None, accept_new_options=True, key="class_input")
+        st.text_input("Subclass", placeholder="Subclass", label_visibility="hidden", key="subclass_input")
+        st.text_input("Background", placeholder="Background", label_visibility="hidden", key="subclass_input")
+        st.space()
+        st.number_input("Armor Class", placeholder="Armor Class", label_visibility="hidden", value=None, step=0, key="ac_input")
+        st.number_input("HP Max", placeholder="HP Max", label_visibility="hidden", value=None, step=0, key="hp_max")
+        st.number_input("Speed", placeholder="Speed", label_visibility="hidden", value=None, step=0, key="speed_input")
+        st.write("<h6> Ability Scores </h6")
         st.form_submit_button('Add Character', on_click=form_callback)
