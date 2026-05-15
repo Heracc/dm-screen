6251a6a49d4694e1e1eb862018dea8da0c3e8175
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-15 12:03:41 -0700

    highcoupenuienly spelled container wrong

--- pages/02players.py
+++ pages/02players.py
@@ -24,7 +24,7 @@ with st.expander("Add a Player"):
         st.number_input("HP Max", placeholder="HP Max", label_visibility="hidden", min_value=0, step=1, key="hp_max")
         st.number_input("Speed", placeholder="Speed", label_visibility="hidden", min_value=0, step=1, key="speed_input")
         st.markdown("###### Ability Scores")
-        with st.containter(horizontal=True):
+        with st.container(horizontal=True):
             st.number_input("STR", placeholder="STR", label_visibility="hidden", min_value=0, step=1, width=100, key="strength_input")
             st.number_input("STR", placeholder="DEX", label_visibility="hidden", min_value=0, step=1, width=100, key="dexterity_input")
             st.number_input("STR", placeholder="CON", label_visibility="hidden", min_value=0, step=1, width=100, key="constitution_input")
