4d3a9dacfc41d29ae106c8ab30352669c7cb0545
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-21 20:40:36 -0700

    fixing forms not having submits and also fixing the callbacks

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,20 @@ from supabase_client import supabase
 if "user" not in st.session_state:
     st.session_state.user = None
 
-def sign_up(email, password):
+def sign_up():
     response = supabase.auth.sign_up(
     {
-        "email": email,
-        "password": password
+        "email": st.session_state.su_email_input,
+        "password": st.session_state.su_password_input
     })
     st.write(f"heres the user id: {response[id]}")
     st.session_state.user = response[id]
 
-def sign_in(email, password):
+def sign_in():
     response = supabase.auth.sign_in_with_password(
     {
-        "email": email,
-        "password": password
+        "email": st.session_state.si_email_input,
+        "password": st.session_state.si_password_input
     })
     st.session_state.user = response[id]
 
@@ -29,14 +29,14 @@ if st.session_state.user == None:
     st.write("Sign in to access player sheet storage. \n You can use the initiative tracker and bestiary without an account, though :)")
     with st.form("sign_up"):
         st.write("Sign Up")
-        su_email_input = st.text_input("Email")
-        su_password_input = st.text_input("Password; minimum 6 characters", type="password")
-        st.sign_up(su_email_input, su_password_input)
+        st.text_input("Email", key="su_email_input")
+        st.text_input("Password; minimum 6 characters", type="password", key="su_password_input")
+        st.form_submit_button("Sign Up", on_click=sign_up)
     with st.form("sign_in"):
         st.write("Sign In")
-        si_email_input = st.text_input("Email")
-        si_password_input = st.text_input("Password; minimum 6 characters", type="password")
-        st.sign_up(si_email_input, si_password_input)
+        st.text_input("Email", key="si_email_input")
+        st.text_input("Password; minimum 6 characters", type="password", key="si_password_input")
+        st.form_submit_button("Sign In", on_click=sign_in)
 else:
     st.write("Welcome back!")
     st.write(f"user id: {st.session_state.user}")
