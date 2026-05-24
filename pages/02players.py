f12fc4f93818725722554b3d85bafaa8ada59e2a
Heracc <73125351+Heracc@users.noreply.github.com>
2026-05-24 10:01:24 -0700

    trying to remove number column headers in transposed df

--- pages/02players.py
+++ pages/02players.py
@@ -154,6 +154,7 @@ if st.button("retrieve"):
     with Session(engine) as session:
         df = pd.read_sql_query(session.query(Players).filter(Players.user_id == st.session_state.user).statement, session.connection())
         df.drop(columns=['id', 'user_id'], inplace=True)
+        df.set_index("name", inplace=True)
         transposed_df = df.T
         st.dataframe(df)
         st.dataframe(transposed_df)
