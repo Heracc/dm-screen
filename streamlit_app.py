0c1e3175b1f0908d53563c35192f80dcfee3ae01
Heracc <73125351+Heracc@users.noreply.github.com>
2026-06-01 15:03:03 -0700

    adding the pages back for evan

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ if st.session_state.user == None:
     pg = st.navigation([account_page, initiative_page, bestiary_page])
 elif st.session_state.user == "cbaee7e9-cbd4-4624-b013-af943fd21702":
     bestiary_page = st.Page("pages/03bestiary.py", title="bestiality (for goonmaster45's eyes only)")
+    pg = st.navigation([account_page, initiative_page, players_page, bestiary_page])
 else:
     pg = st.navigation([account_page, initiative_page, players_page, bestiary_page])
 pg.run()
