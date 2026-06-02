0fe97d9dff5977dabe292fd9263bdeaa86fcb520
Heracc <73125351+Heracc@users.noreply.github.com>
2026-06-02 08:59:12 -0700

    renaming bestiary page

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 account_page = st.Page("pages/00account.py", title="Account")
 initiative_page = st.Page("pages/01initiative.py", title="Initative Tracker")
 players_page = st.Page("pages/02players.py", title="Players")
-bestiary_page = st.Page("pages/03bestiary.py", title="Bestiary")
+creatures_page = st.Page("pages/03creatures.py", title="Creatures")
 
 if "user" not in st.session_state:
     st.session_state.user = None
@@ -11,8 +11,8 @@ if "user" not in st.session_state:
 if st.session_state.user == None:
     pg = st.navigation([account_page, initiative_page, bestiary_page])
 elif st.session_state.user == "cbaee7e9-cbd4-4624-b013-af943fd21702":
-    bestiary_page = st.Page("pages/03bestiary.py", title="bestiality (for goonmaster45's eyes only)")
-    pg = st.navigation([account_page, initiative_page, players_page, bestiary_page])
+    creatures_page = st.Page("pages/03creatures.py", title="bestiality (for goonmaster45's eyes only)")
+    pg = st.navigation([account_page, initiative_page, players_page, creatures_page)
 else:
-    pg = st.navigation([account_page, initiative_page, players_page, bestiary_page])
+    pg = st.navigation([account_page, initiative_page, players_page, creatures_page])
 pg.run()
