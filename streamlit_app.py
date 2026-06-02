4e2e48100a2257ba34340e91c267d98e7a1ac38f
Heracc <73125351+Heracc@users.noreply.github.com>
2026-06-02 09:01:36 -0700

    replacing a ] that i accidentially deleted from the list of pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ if st.session_state.user == None:
     pg = st.navigation([account_page, initiative_page, bestiary_page])
 elif st.session_state.user == "cbaee7e9-cbd4-4624-b013-af943fd21702":
     creatures_page = st.Page("pages/03creatures.py", title="bestiality (for goonmaster45's eyes only)")
-    pg = st.navigation([account_page, initiative_page, players_page, creatures_page)
+    pg = st.navigation([account_page, initiative_page, players_page, creatures_page])
 else:
     pg = st.navigation([account_page, initiative_page, players_page, creatures_page])
 pg.run()
