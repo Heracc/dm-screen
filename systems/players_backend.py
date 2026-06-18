ec19b31634a5a19644514ca54200104b2c961127
Heracc <73125351+Heracc@users.noreply.github.com>
2026-06-18 11:11:55 -0700

    moving players model to a shared module

---
+++ systems/players_backend.py
@@ -0,0 +1,51 @@
+from decouple import config
+import uuid
+from sqlalchemy import create_engine, Column, Integer, Text, Uuid, JSON, Computed
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.pool import NullPool
+
+USER = config("DB_USER")
+PASSWORD = config("DB_PASSWORD")
+DBNAME = config("DB_NAME")
+PORT = config("DB_PORT")
+HOST = config("DB_HOST")
+
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+def mod_calc(score):
+    return (score-10)//2
+
+class Base(DeclarativeBase):
+    pass
+
+class Players(Base):
+    __tablename__ = "players"
+
+    id = Column(Uuid, primary_key=True, default=uuid.uuid4)
+    user_id = Column(Uuid)
+    name = Column(Text, default="")
+    race = Column(Text, default="")
+    _class = Column(Text, default="")
+    subclass = Column(Text, default="")
+    background = Column(Text, default="")
+    level = Column(Integer, default=1)
+    hp = Column(Integer, default=0)
+    ac = Column(Integer, default=0)
+    speed = Column(Integer, default=0)
+    str = Column(Integer, default=3)
+    dex = Column(Integer, default=3)
+    con = Column(Integer, default=3)
+    int = Column(Integer, default=3)
+    wis = Column(Integer, default=3)
+    cha = Column(Integer, default=3)
+    str_mod = Column(Integer, Computed(mod_calc(str), persisted=True))
+    dex_mod = Column(Integer, Computed(mod_calc(dex), persisted=True))
+    con_mod = Column(Integer, Computed(mod_calc(con), persisted=True))
+    int_mod = Column(Integer, Computed(mod_calc(wis), persisted=True))
+    wis_mod = Column(Integer, Computed(mod_calc(int), persisted=True))
+    cha_mod = Column(Integer, Computed(mod_calc(cha), persisted=True))
+    ## AI told me how to make this a list stored in the column
+    languages = Column(JSON)
+
+
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
